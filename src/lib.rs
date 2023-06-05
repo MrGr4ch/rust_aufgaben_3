938c4501c06f5c3bdf16ecb8d9fa74448e724be3
Benjamin Brendli <61924736+benjaminbrendli@users.noreply.github.com>
2023-06-05 16:15:20 +0200

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -7,7 +7,5 @@ pub type ParseResult = Result<(), String>;
 pub use lexer::C1Lexer;
 pub use lexer::C1Token;
 
-// You will need a re-export of your C1Parser definition. Here is an example:
 mod parser;
-mod c1_parser;
 pub use parser::C1Parser;
