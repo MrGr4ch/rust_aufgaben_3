aefe1e57a3a90ea0452247675d42a5bead6d9aee
Benjamin Brendli <61924736+benjaminbrendli@users.noreply.github.com>
2023-06-05 16:42:54 +0200

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 mod lexer;
+mod parser;
 
 // Type definition for the Result that is being used by the parser. You may change it to anything
 // you want
@@ -6,6 +7,4 @@ pub type ParseResult = Result<(), String>;
 
 pub use lexer::C1Lexer;
 pub use lexer::C1Token;
-
-mod parser;
 pub use parser::C1Parser;
